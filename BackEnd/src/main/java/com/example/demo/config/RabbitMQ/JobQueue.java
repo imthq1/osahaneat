eb26c6faad70e0ae8f6607ca3086016df506d6b4
Nguyen Toan Thang <xamthang09@gmail.com>
2025-06-22 21:51:30 +0700

    fix listener Rabbit

--- BackEnd/src/main/java/com/example/demo/config/RabbitMQ/JobQueue.java
+++ BackEnd/src/main/java/com/example/demo/config/RabbitMQ/JobQueue.java
@@ -4,9 +4,10 @@ import java.util.Arrays;
 import java.util.List;
 
 public class JobQueue {
-    public static final String QUEUE_DEV = "rabbit-queue-order";
+    public static final String QUEUE_PROCESS = "rabbit-queue";
     public static final String QUEUE_DEV_REGISTER = "rabbit-queue-register";
+    public static final String QUEUE_DEV_ORDER = "rabbit-queue-order";
 
-    public static final List<String> queueNameList = Arrays.asList(QUEUE_DEV, QUEUE_DEV_REGISTER);
+    public static final List<String> queueNameList = Arrays.asList(QUEUE_PROCESS, QUEUE_DEV_REGISTER,QUEUE_DEV_ORDER);
 }
 
